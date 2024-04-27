c5e705efe2a5f856b2f1c1cd56d521711f7f4d4a
jamiesone <jamiesone@outlook.com>
2024-04-27 13:12:36 +0200

    updating rsvp

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,8 +2,10 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
+   empty($_POST['numberSelection']) 		||
    empty($_POST['message'])	||
+   empty($_POST['attendance'])	||
+   empty($_POST['evening'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
@@ -12,15 +14,30 @@ if(empty($_POST['name'])  		||
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['phone'];
+$attendance = $_POST['attendance'];
 $message = $_POST['message'];
+$evening = $_POST['evening'];
+$numberSelection = $_POST['numberSelection'];
 
-// Create the email and send the message
-$to = 'jerome.lachaud@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+$con = mysqli_connect('emilyandpascal.cnc28ymq8qzp.eu-west-1.rds.amazonaws.com', 'admin', 'emilyetsonDef2024!','rsvp');
+$sql = "INSERT INTO `guests` (`name`, `email`, `attendance`, `numberSelection`, `message`, `evening`) VALUES ('$name', '$email', '$attendance', '$numberSelection', '$message', '$evening');"
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	return true;
+}
+else{
+   return false;
+}
+
+
+// // Create the email and send the message
+// $to = 'jerome.lachaud@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";
+// mail($to,$email_subject,$email_body,$headers);
+// return true;
 ?>
